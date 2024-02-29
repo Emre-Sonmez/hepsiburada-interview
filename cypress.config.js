a036a70e1ebb291ff9a4ff999a10cb0d79d6e259
Emre-Sonmez <mail.emresonmez@gmail.com>
2024-02-29 17:07:11 +0300

    video failure

--- cypress.config.js
+++ cypress.config.js
@@ -2,7 +2,8 @@ const { defineConfig } = require("cypress");
 
 module.exports = defineConfig({
 
-  video: true,
+  video: false,
+  videoUploadOnPasses: false,
   e2e: {
     setupNodeEvents(on, config) {
       // implement node event listeners here
