a8764638c099fcb83cde80e8d74361e91d080dcf
Emre-Sonmez <mail.emresonmez@gmail.com>
2024-02-29 18:01:24 +0300

    video update

--- cypress.config.js
+++ cypress.config.js
@@ -3,24 +3,14 @@ const fs = require('fs')
 
 module.exports = defineConfig({
 
+  videoUploadPath: "cypress/videos",
   video: true,
   videoCompression: true,
   // setupNodeEvents can be defined in either
   // the e2e or component configuration
   e2e: {
     setupNodeEvents(on, config) {
-      on('after:spec', (spec, results) => {
-        if (results && results.video) {
-          // Do we have failures for any retry attempts?
-          const failures = results.tests.some((test) =>
-            test.attempts.some((attempt) => attempt.state === 'failed')
-          )
-          if (!failures) {
-            // delete the video if the spec passed and no tests retried
-            fs.unlinkSync(results.video)
-          }
-        }
-      })
+
     },
   },
 })
---
+++ cypress/support/index.js
@@ -0,0 +1,17 @@
+const fs = require("fs-extra");
+const path = require("path");
+
+module.exports = (on, config) => {
+  on("after:spec", (spec, results) => {
+    if (results && results.video) {
+      // Do we have failures for any retry attempts?
+      const failures = results.tests.some((test) =>
+        test.attempts.some((attempt) => attempt.state === "failed")
+      );
+      if (!failures) {
+        // delete the video if the spec passed and no tests retried
+        fs.unlinkSync(results.video);
+      }
+    }
+  });
+};
