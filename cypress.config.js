f1eeb9601221f4f7a9b36a236e2db1045c76884e
Emre-Sonmez <mail.emresonmez@gmail.com>
2024-02-29 17:29:59 +0300

    Update cypress.config.js

--- cypress.config.js
+++ cypress.config.js
@@ -4,6 +4,8 @@ module.exports = defineConfig({
 
   video: true,
   videoCompression: true,
+  videoUploadOnPasses: false,
+
   e2e: {
     setupNodeEvents(on, config) {
       // implement node event listeners here
