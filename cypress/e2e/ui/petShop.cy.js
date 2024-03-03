6eed948239d48ef523d085b7791545960e2c0d6e
Emre-Sonmez <mail.emresonmez@gmail.com>
2024-03-03 12:24:50 +0300

    Update petShop.cy.js

--- cypress/e2e/ui/petShop.cy.js
+++ cypress/e2e/ui/petShop.cy.js
@@ -7,18 +7,40 @@ describe("Pet Shop", () => {
     cy.visit(
       "https://www.google.com/url?sa=D&q=https://www.hepsiburada.com/kampanyalar/yurt-disindan-urunler%3Fwt_int%3Dhytop.yurtdisi.kampanya&ust=1709380440000000&usg=AOvVaw1JyjtbUOMEqMG-A2vz6wx4&hl=tr&source=gmail"
     );
+    cy.get(
+      '[href="https://www.hepsiburada.com/kampanyalar/yurt-disindan-urunler?wt_int=hytop.yurtdisi.kampanya"]'
+    )
+      .its("length")
+      .then((length) => {
+        if (length > 0) {
+          cy.get(
+            '[href="https://www.hepsiburada.com/kampanyalar/yurt-disindan-urunler?wt_int=hytop.yurtdisi.kampanya"]'
+          ).click();
+        } else {
+          cy.log("No element found");
+        }
+      });
+
     cy.wait(10000);
     cy.get("#myAccount").realHover("mouse");
     cy.get("#login").click({ force: true });
     cy.wait(10000);
-    cy.get('#txtUserName', {timeout:60000}).should('be.visible').type('hepsiburada.interview@gmail.com')
-    cy.get('#btnLogin').should('be.visible').click()
-    cy.get('#txtPassword').should('be.visible').type('Test123!*')
-    cy.get('#btnEmailSelect').should('be.visible').click()
-    cy.get("#login").click({ force: true });
+    cy.get("#txtUserName", { timeout: 60000 })
+      .should("be.visible")
+      .type("hepsiburada.interview@gmail.com");
+    cy.get("#btnLogin").should("be.visible").click();
+    cy.get("#txtPassword").should("be.visible").type("Test123!*");
+    cy.get("#btnEmailSelect").should("be.visible").click();
   });
 
   it("Add a pet shop product to cart and check", () => {
-    cy.wait(1000);
+    cy.get(
+      "div#container div.sf-TopLinks-BNeUkuZpKh9UMA2_zK8p > ul > li:nth-child(3) > a"
+    ).click();
+    cy.wait(10000);
+    cy.scrollTo("center");
+    cy.get(
+      "div:nth-of-type(17) .treeCategoryContent-XPVj5InCxOWIJtyTC35Z"
+    ).click({ force: "true" });
   });
 });
