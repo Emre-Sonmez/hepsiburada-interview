626c23e7b4e4c01661f36cd8bce4fc69f75a983d
Emre-Sonmez <mail.emresonmez@gmail.com>
2024-03-03 19:43:08 +0300

    add ui scenarios 2

---
+++ cypress/pageObjectModel/loginPage.js
@@ -0,0 +1,19 @@
+export class loginPage {
+  get usernameField() {
+    return cy.get("#txtUserName");
+  }
+
+  get loginBtnUsername() {
+    return cy.get("#btnLogin");
+  }
+
+  get passwordField() {
+    return cy.get("#txtPassword");
+  }
+
+  get loginBtnPassword() {
+    return cy.get("#btnEmailSelect");
+  }
+}
+
+export default new loginPage();
