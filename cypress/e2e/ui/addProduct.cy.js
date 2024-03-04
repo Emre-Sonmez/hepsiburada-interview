f4556ccddf5580fd8bd41d7c0ae440b1a19607a9
Emre-Sonmez <mail.emresonmez@gmail.com>
2024-03-04 10:54:51 +0300

    change pom

--- cypress/e2e/ui/addProduct.cy.js
+++ cypress/e2e/ui/addProduct.cy.js
@@ -1,16 +1,25 @@
 /// <reference types="cypress" />
 import "cypress-real-events/support";
-import { homepage } from "../../pageObjectModel/homepage";
-import { shoppingCartPage } from "../../pageObjectModel/shoppingCartPage";
-const shoppingCartPagePom = new shoppingCartPage();
-const homepagePom = new homepage();
+import homepage from "../../pageObjectModel/homepage";
+import yurtDisindanPage from "../../pageObjectModel/yurtDisindanPage";
+import shoppingCartPage from "../../pageObjectModel/shoppingCartPage";
 
 describe("Add Product", () => {
   beforeEach(() => {
     cy.log("Test starts");
-    homepagePom.navigate();
-    homepagePom.urlRedirectControl();
-    homepagePom.goLoginPage();
+    cy.wait(3000);
+    cy.visit(
+      "https://www.google.com/url?sa=D&q=https://www.hepsiburada.com/kampanyalar/yurt-disindan-urunler%3Fwt_int%3Dhytop.yurtdisi.kampanya&ust=1709380440000000&usg=AOvVaw1JyjtbUOMEqMG-A2vz6wx4&hl=tr&source=gmail"
+    );
+    cy.urlRedirectControl();
+    cy.wait(5000);
+    homepage.girisYapVeyaUyeOlButton.realHover("mouse");
+    homepage.girisYapButton
+      .should("be.visible")
+      .should("contain.text", "Giriş Yap")
+      .click({ force: true });
+    cy.url().should("contains", "https://giris.hepsiburada.com/");
+    cy.wait(5000);
   });
 
   it("Add a product to cart and check", () => {
@@ -19,10 +28,22 @@ describe("Add Product", () => {
     cy.get("@credentials").then((data) => {
       cy.login(data.username, data.password);
     });
+    cy.goProductsFromAbroadPage();
+    yurtDisindanPage.productCart.eq(13).realHover("mouse");
+    homepage.sepeteEkleButton
+      .should("be.visible")
+      .should("contain.text", "Sepete ekle")
+      .click({ force: true });
+    homepage.sepeteEkleButton
+      .should("be.visible")
+      .should("contain.text", "Sepete ekle")
+      .click({ force: true });
+    cy.contains("Ürün sepete eklendi").should("be.visible");
+    homepage.goShoppingCartPage.should("exist").click({ force: true });
+    cy.url().should("contains", "https://checkout.hepsiburada.com/sepetim");
 
-    homepagePom.goProductsFromAbroadPage();
-    homepagePom.addFirstProductToCart();
-    homepagePom.goShoppingCartPage();
-    shoppingCartPagePom.deleteButton();
+    shoppingCartPage.deleteProductButton.should("be.visible").click({ force: true });
+    cy.deletePopupControl();
+    cy.contains("Ürün sepetinizden kaldırıldı").should("be.visible");
   });
 });
--- cypress/support/commands.js
+++ cypress/support/commands.js
@@ -6,3 +6,41 @@ Cypress.Commands.add("login", (username, password) => {
   loginPage.passwordField.type(password);
   loginPage.loginBtnPassword.click();
 });
+
+Cypress.Commands.add("urlRedirectControl", () => {
+  cy.get(
+    '[href="https://www.hepsiburada.com/kampanyalar/yurt-disindan-urunler?wt_int=hytop.yurtdisi.kampanya"]'
+  )
+    .if("visible")
+    .then(() => {
+      cy.get(
+        '[href="https://www.hepsiburada.com/kampanyalar/yurt-disindan-urunler?wt_int=hytop.yurtdisi.kampanya"]'
+      ).click({ force: true });
+    })
+    .else()
+    .log("No element found");
+});
+
+Cypress.Commands.add("goProductsFromAbroadPage", () => {
+  cy.get(
+    'a[href="https://www.hepsiburada.com/kampanyalar/yurt-disindan-urunler?wt_int=hytop.yurtdisi.kampanya"]'
+  )
+    .should("be.visible")
+    .click({ force: true });
+
+  cy.url().should(
+    "contains",
+    "https://www.hepsiburada.com/kampanyalar/yurt-disindan-urunler"
+  );
+});
+
+
+Cypress.Commands.add("deletePopupControl", () => {
+      cy.get('[kind="secondary"]')
+      .if("visible")
+      .then(() => {
+        cy.get('[kind="secondary"]').click({ force: true });
+      })
+      .else()
+      .log("No element found");
+});
